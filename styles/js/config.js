b92f9f45d4403dcda408b1f0b277895e41d49258
Charter <ch.art.reuse.uwu@gmail.com>
2026-03-09 17:23:08 +0800

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -15,9 +15,9 @@ let charadex = {};
 /* Any preview links will still show Charadex's information
 /* ==================================================================== */
 charadex.site = {
-  title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
-  description: `A tool for organizing small ARPGs and species.`
+  title: "HAKOPONS",
+  url: "https://hakopons.netlify.app/",
+  description: `Masterlist for HAKOPONS`
 }
 
 /* ==================================================================== */
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1rwSinnDMOGGFfl_dafSuP-tJx3T5Wj6RD-qN7cKArXo",
 
   pages: {
     masterlist:    "masterlist",
